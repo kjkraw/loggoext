d8bfe062b8da8f8033a152b4001aaee6f7772535
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2020-09-01 20:39:08 -0700

    Properly handle root logger

--- debug.go
+++ debug.go
@@ -1,17 +1,21 @@
 package loggoext
 
 import (	
+	"fmt"
 	"github.com/juju/loggo"
 )
 
 func DebugLoggers(debugModules []string) {
 	conf := loggo.Config{
-		"<root>": loggo.INFO,
+		"": loggo.INFO,
 	}
 	for _, mod := range debugModules {
+		if mod == "<root>":
+			mod = ""
 		conf[mod] = loggo.DEBUG
 	}
 
+	fmt.Printf("Setting logging level to debug for modules: %v", debugModules)
 	err := loggo.ConfigureLoggers(conf.String())
 	if err != nil {
 		panic(err)
